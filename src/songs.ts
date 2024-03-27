0419dfb7acbc7f31589c6fb5f1d3eddf3b4eed9d
MarcosRodriguezAlonso <161734470+MarcosRodriguezAlonso@users.noreply.github.com>
2024-03-27 16:13:50 +0100

    Delete comment

--- src/songs.ts
+++ src/songs.ts
@@ -2,31 +2,22 @@ import { errorMonitor } from "events";
 import { Song } from "./songsData";
 
 export const areSameSong = (song: Song, songTitle: string): boolean => {
-  // Asígnale a la variable areSame un true o un false dependiendo de si
-  // el título de la canción recibida coincide con el título recibido
+
   return song.title === songTitle;
 };
 
 export const addSong = (song: Song, songs: Song[]): void => {
-  // Añade la canción recibida al array de canciones recibido
+
   songs.push(song);
 };
 
 export const isPlaylistFull = (songs: Song[]): boolean => {
-  // Asígnale a la variable isFull un true o un false dependiendo de si
-  // el array de títulos recibido tiene 4 o más elementos
+
   return songs.length >= 4;
 };
 
 export const getErrorMessage = (errorCode: string): string => {
   let errorMessage: string;
-
-  // A la variable anterior le tendrás que asignar un mensaje de error
-  // dependiendo del código de error recibido
-  // Si el código de error no es válido, asígnale un mensaje genérico
-  // Éstos son los códigos de error y sus mensajes correspondientes:
-  // - "exists": "La canción ya existe"
-  // - "limit": "La playlist está llena
 
   switch (errorCode) {
     case "exists":
@@ -43,12 +34,12 @@ export const getErrorMessage = (errorCode: string): string => {
 };
 
 export const getSongsCount = (songs: Song[]): number => {
-  // Asígnale a la variable songsCount el número de canciones recibidas
+
   return songs.length;
 };
 
 export const removeSongByPosition = (songs: Song[], position: number): void => {
-  // Elimina del array de canciones recibido la canción que se encuentra en la posición recibida
+
   songs.splice(position, 1);
 };
 
@@ -61,10 +52,7 @@ export const getNextSongPosition = (
   currentSongPosition: number
 ) => {
   let nextSongPosition: number;
-  // En currentSongPosition tienes el índice de la canción que se está reproduciendo
-  // En songs tienes el array de canciones
-  // Asígnale a la variable nextSongPosition el índice de la siguiente canción que se tiene que reproducir
-  // Si la canción actual es la última, la siguiente será la primera
+
   nextSongPosition = currentSongPosition + 1;
 
   if (nextSongPosition >= songs.length) {
@@ -76,8 +64,7 @@ export const getNextSongPosition = (
 
 export const isCurrentSong = (song: Song): boolean => {
   let isCurrent: boolean;
-  // Asígnale a la variable isCurrent un true o un false dependiendo de si
-  // la canción recibida es la actual
+
 
   return song.isCurrent === true;
 };
